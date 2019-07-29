f26fdb7ed6aa0264e8798d1053bce705b2baafb4
shiyangyang <shiyangyang@flowpp.com>
2019-07-29 16:51:11 +0800

    README

--- .f2econfig.js
+++ .f2econfig.js
@@ -1,6 +1,12 @@
 const { argv } = process
 const build = argv[argv.length - 1] === 'build'
 const { join } = require('path')
+const { readFileSync, existsSync } = require('fs')
+
+let password = undefined
+if (existsSync(join(__dirname, '.password'))) {
+    password = readFileSync(join(__dirname, '.password')).toString()
+}
 
 module.exports = {
     livereload: !build,
@@ -9,7 +15,7 @@ module.exports = {
     useLess: true,
     buildFilter: p => /^test/.test(p),
     middlewares: [
-        require('./')
+        (conf) => require('./')(conf, { password })
     ],
     output: join(__dirname, './docs'),
 }
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # f2e-middle-markdown-ppt
 markdown-ppt for f2e-server
+
+## Usage
+
+`npm i f2e-middle-markdown-ppt`
+
+`.f2econfig.js`
+```
+{
+    ...conf,
+    middlewares: [
+        { middleware: 'markdown-ppt', password: 'iamspeaker yeh!' }
+    ]
+}
+```
+
+`npm start`
+
+## View
+1. visit `.ppt.md` replace with `.html`.
+2. use `?mode=speaker` to handle page prev/next.
+3. enjoy!
